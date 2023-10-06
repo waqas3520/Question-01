413844a88f80c8f4a6b95d4252f4f8746ca328e8
Muhammad Waqas <139634396+waqas3520@users.noreply.github.com>
2023-10-06 11:36:47 +0500

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,9 @@
+"use strict";
+//Name	Muhammad  Waqas
+//Roll No	PIAIC215686
+//Batch	Batch 47
+// Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let Name; // variable declaration
+Name = "Waqas"; // value assignment
+console.log(`Hello ${Name}, would you like to learn some TypeScript today?`);
---
+++ q2.ts
@@ -0,0 +1,14 @@
+//Name	Muhammad  Waqas
+//Roll No	PIAIC215686
+//Batch	Batch 47
+
+// Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+
+let Name: string; // variable declaration
+Name = "Waqas"; // value assignment
+
+
+console.log(`Hello ${Name}, would you like to learn some TypeScript today?`);
